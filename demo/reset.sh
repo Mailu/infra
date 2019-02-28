935f4a9d1c68f6ea156149163e33d1b579d7695b
Tim Möhlmann <muhlemmer@gmail.com>
2019-02-28 21:38:58 +0200

    Cron proofing

--- demo/reset.sh
+++ demo/reset.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /opt/infra/demo
-docker-compose down || exit 1
+/usr/bin/docker-compose down || exit 1
 rm -rf /mailu
-docker-compose up -d || exit 1
+/usr/bin/docker-compose up -d || exit 1
 sleep 30
-docker-compose exec admin flask mailu admin admin test.mailu.io letmein || exit 1
+/usr/bin/docker-compose exec admin flask mailu admin admin test.mailu.io letmein || exit 1
