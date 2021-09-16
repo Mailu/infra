68c0f77893ac5f59d8eff6665733f18e4ad23539
Alexander Graf <ghostwheel42@users.noreply.github.com>
2021-09-16 10:53:19 +0200

    also re-create admin if it was deleted

--- demo/admin-pw.sh
+++ demo/admin-pw.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /opt/infra/demo
-/usr/local/bin/docker-compose exec admin flask mailu password admin test.mailu.io letmein >/dev/null
+/usr/local/bin/docker-compose exec admin flask mailu admin --mode update admin test.mailu.io letmein >/dev/null
