db3d28b78c6f9c9c4cc450fb332121d361f942bf
Alexander Graf <ghostwheel42@users.noreply.github.com>
2021-09-16 10:08:43 +0200

    fixed reset of admin account password
    - set via INITIAL_ADMIN_* on update and on reset
    - admin-pw.sh: set without -d to log errors

--- demo/reset.sh
+++ demo/reset.sh
@@ -4,5 +4,3 @@ cd /opt/infra/demo
 /usr/local/bin/docker-compose down || exit 1
 rm -rf /mailu
 /usr/local/bin/docker-compose up -d || exit 1
-sleep 30
-/usr/local/bin/docker-compose exec -d admin flask mailu admin admin test.mailu.io letmein || exit 1
