cb896e4ddf19fc30e6eee5383a73c3c44e8d7bb4
Dimitri Huisman <diman@huisman.xyz>
2023-09-27 13:21:21 +0000

    Update scripts to use 'docker compose' and update the admin password.

--- demo/admin-pw.sh
+++ demo/admin-pw.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /opt/infra/demo
-/usr/local/bin/docker-compose exec admin flask mailu admin --mode update admin test.mailu.io letmein >/dev/null
+/usr/bin/docker compose exec admin flask mailu admin --mode update admin test.mailu.io MailuDemo >/dev/null
--- demo/reset.sh
+++ demo/reset.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm -rf /mailu
 cd /opt/infra/demo
-/usr/local/bin/docker-compose down || exit 1
+/usr/bin/docker compose down || exit 1
 rm -rf /mailu
-/usr/local/bin/docker-compose up -d || exit 1
+/usr/bin/docker compose up -d || exit 1
